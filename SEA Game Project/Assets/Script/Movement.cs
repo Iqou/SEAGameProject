122acc3ecd7b1c1816cc9488aeda02ff561a7d29
rmarv <rmarvaputra@gmail.com>
2025-05-14 23:53:02 +0700

    Input Asset

--- SEA Game Project/Assets/Script/Movement.cs
+++ SEA Game Project/Assets/Script/Movement.cs
@@ -15,7 +15,7 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
