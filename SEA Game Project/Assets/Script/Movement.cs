f3e4615ca854b0f05fda32f8fd0b1499aa8bdb0e
rmarv <rmarvaputra@gmail.com>
2025-05-14 15:47:40 +0700

    Test Buat
    gabut berhadiah

---
+++ SEA Game Project/Assets/Script/Movement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5;
+    public int facing = 1;
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if(horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+        flip();
+        }
+
+        anim.SetFloat("horizontal",Mathf.Abs(horizontal));
+        anim.SetFloat("vertical",Mathf.Abs(vertical));
+
+        rb.linearVelocity = new Vector2(horizontal,vertical) * speed;
+    }
+
+    void flip()
+    {
+        facing *= -1;
+        transform.localScale = new Vector3(transform.localScale.x*-1, transform.localScale.y, transform.localScale.z);
+    }
+}
+    
