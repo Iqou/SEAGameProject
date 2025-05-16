da17eb399b82a94ff27026225e9e91e8138335d8
Artyrdrom <rafie0635@gmail.com>
2025-05-16 23:18:03 +0700

    a

--- SEA Game Project/Assets/Script/CharacterHealth.cs
+++ SEA Game Project/Assets/Script/CharacterHealth.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
-public class NewEmptyCSharpScript
+
+public class PlayerHealth : MonoBehaviour
 {
-    
+    public int currentHealth;
+    public int maxHealth;
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth += amount;
+    }
 }
