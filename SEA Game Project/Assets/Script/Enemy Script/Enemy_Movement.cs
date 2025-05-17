688b62c6f7415529619681a2cfa533797ec6c7b2
MonyetttRindam <khoiriabil13@gmail.com>
2025-05-18 03:10:13 +0700

    enemy_attack

--- SEA Game Project/Assets/Script/Enemy Script/Enemy_Movement.cs
+++ SEA Game Project/Assets/Script/Enemy Script/Enemy_Movement.cs
@@ -5,10 +5,17 @@ using System.Collections.Generic;
 public class EnemyMovement : MonoBehaviour 
 {
     public float speed;
-    private int facingDirection = -1;
-    public EnemyState enemyState;
+    public float attackRange = 2;
+    public float attackCooldown = 2;
+    public float playerDetectRange = 5;
+    public Transform detectionPoint;
+    public LayerMask playerLayer;
 
-    public float attackRange = 3;
+
+    private float attackCooldownTimer;
+    private int facingDirection = -1;
+    private EnemyState enemyState;
+
 
     private Rigidbody2D rb;
     private Transform player;
@@ -26,25 +33,28 @@ public class EnemyMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CheckForPlayer();
+
+        if (attackCooldownTimer > 0)
+        {
+            attackCooldownTimer -= Time.deltaTime;
+        }
         if (enemyState == EnemyState.Chasing)
         {
             Chase();
         }
         else if(enemyState == EnemyState.Attacking)
         {
+            rb.linearVelocity = Vector2.zero;  
 
         }
     }
 
     void Chase()
     {
-        if (Vector2.Distance(transform.position, player.transform.position) <= attackRange)
-        {
-            ChangeState(EnemyState.Attacking);
-        }
-        else if(player.position.x > transform.position.x && facingDirection == -1 ||
+        if(player.position.x > transform.position.x && facingDirection == -1 ||
                 player.position.x < transform.position.x && facingDirection == 1)
-            {
+        {
             Flip();
         }
         Vector2 direction = (player.position - transform.position).normalized;
@@ -56,26 +66,33 @@ public class EnemyMovement : MonoBehaviour
         transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 
-    
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void CheckForPlayer()
     {
-        if (collision.gameObject.tag == "Player")
+        Collider2D[] hits = Physics2D.OverlapCircleAll(detectionPoint.position, playerDetectRange, playerLayer);
+
+        if (hits.Length > 0)
         {
-            if (player == null)
+            player = hits[0].transform;
+
+            if (Vector2.Distance(transform.position, player.position) < attackRange && attackCooldownTimer <= 0)
             {
-                player = collision.transform;
+                attackCooldownTimer = attackCooldown;
+                ChangeState(EnemyState.Attacking);
             }
-            ChangeState(EnemyState.Chasing);
+
+            else if (Vector2.Distance(transform.position, player.position) > attackRange)
+            {
+                ChangeState(EnemyState.Chasing);
+            }
         }
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        else
         {
             rb.linearVelocity = Vector2.zero;
             ChangeState(EnemyState.Idle);
-        }
+        }          
     }
+    
 
     void ChangeState(EnemyState newState)
     {
@@ -98,6 +115,12 @@ public class EnemyMovement : MonoBehaviour
         anim.SetBool("IsChasing",true);
         else if (enemyState == EnemyState.Attacking)
             anim.SetBool("IsAttacking", true);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionPoint.position, playerDetectRange);
     }
 }
 
