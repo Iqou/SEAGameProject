7d511e8779e50a53e16c97878c6a69059899cbe6
Iqou <inigo.siswanto12@gmail.com>
2025-05-17 11:28:26 +0700

    Changes in Description
    Organized a few files such as animations into a folder. Added animations to Orc and fixed Orc layering. Made a few map changes.
    Notes: Animation is still clunky when it switches X axis.

---
+++ SEA Game Project/Assets/Script/Enemy Script/EnemyMovement.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMovement : MonoBehaviour 
+{
+    public float speed;
+    private int facingDirection = -1;
+    public EnemyState enemyState;
+
+    private Rigidbody2D rb;
+    private Transform player;
+    private Animator anim;
+
+    //start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyState == EnemyState.Chasing)
+        {
+            if(player.position.x > transform.position.x && facingDirection == -1||
+                player.position.x < transform.position.x && facingDirection == 1)
+                {
+                    Flip();
+                }
+            Vector2 direction = (player.position - transform.position).normalized;
+            rb.linearVelocity = direction * speed;
+        }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            if (player == null)
+            {
+                player = collision.transform;
+            }
+            ChangeState(EnemyState.Chasing);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            rb.linearVelocity = Vector2.zero;
+            ChangeState(EnemyState.Idle);
+        }
+    }
+
+    void ChangeState(EnemyState newState)
+    {
+
+        //Exit the current animation
+        if(enemyState == EnemyState.Idle)
+            anim.SetBool("IsIdle", false);
+        else if (enemyState == EnemyState.Chasing)
+        anim.SetBool("IsChasing",false);
+
+        //Update our current state
+        enemyState = newState;
+
+        //Update the new animation
+        if(enemyState == EnemyState.Idle)
+            anim.SetBool("IsIdle", true);
+        else if (enemyState == EnemyState.Chasing)
+        anim.SetBool("IsChasing",true);
+    }
+}
+
+public enum EnemyState 
+{
+    Idle,
+    Chasing, 
+}
