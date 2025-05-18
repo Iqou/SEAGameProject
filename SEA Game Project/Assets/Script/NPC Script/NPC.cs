95f6d7dc1f3e42b18c2ba01d5ad2f34785961c6d
rmarv <rmarvaputra@gmail.com>
2025-05-18 17:49:38 +0700

    NPC Area Trigger

--- SEA Game Project/Assets/Script/NPC Script/NPC.cs
+++ SEA Game Project/Assets/Script/NPC Script/NPC.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 public class NPC : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public enum NPCState{Default, Idle, Talk}
+    public NPCState currentState = NPCState.Idle;
+    private NPCState defaultstate;
+
+    public NPC_Talk talk;
+
+
     void Start()
     {
-        
+        defaultstate = currentState;
+        SwitchState(currentState);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SwitchState(NPCState newState)
     {
-        
+        currentState = newState;
+        talk.enabled = newState == NPCState.Talk;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            SwitchState(NPCState.Talk);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player")){
+            SwitchState(defaultstate);
+        }
     }
 }
