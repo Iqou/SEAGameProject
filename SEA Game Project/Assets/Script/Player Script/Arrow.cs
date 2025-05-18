b773bbe3abb1c30b654673094b2dc24b0b62a233
MonyetttRindam <khoiriabil13@gmail.com>
2025-05-18 23:12:41 +0700

    arrow

---
+++ SEA Game Project/Assets/Script/Player Script/Arrow.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 direction = Vector2.right;
+    public float lifeSpawn = 2;
+    public float speed;
+
+    public int damage;
+    public LayerMask enemyLayer;
+    public float knockbackForce;
+    public float knockbackTime;
+    public float stunTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.linearVelocity = direction * speed;
+        RotateArrow();
+        Destroy(gameObject, lifeSpawn);
+    }
+
+    private void RotateArrow()
+    {
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((enemyLayer.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            collision.gameObject.GetComponent<Enemy_Health>().ChangeHealth(-damage);
+            collision.gameObject.GetComponent<Enemy_Knockback>().Knockback(transform, knockbackForce, knockbackTime, stunTime);
+        }
+    }
+
+}
---
+++ SEA Game Project/Assets/Script/Player Script/Player_Bow.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Bow : MonoBehaviour
+{
+    public Transform launchPoint;
+    public GameObject arrowPrefab;
+
+    private Vector2 aimDirection = Vector2.right;
+    public float shootCooldown = .5f;
+    private float shootTimer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        shootTimer -= Time.deltaTime;
+        HandleAiming();
+
+        if (Input.GetButtonDown("Shoot") && shootTimer <= 0)
+        {
+            Shoot();
+        }
+    }
+    private void HandleAiming()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            aimDirection = new Vector2(horizontal, vertical).normalized;
+        }
+    }
+
+
+
+    public void Shoot()
+    {
+        Arrow arrow = Instantiate(arrowPrefab, launchPoint.position, Quaternion.identity).GetComponent<Arrow>();
+        arrow.direction = aimDirection;
+        shootTimer = shootCooldown;
+    }
+}
