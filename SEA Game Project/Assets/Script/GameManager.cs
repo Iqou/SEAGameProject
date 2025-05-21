147cfd5bcda224cba8a0780927922a9919de8313
Artyrdrom <rafie0635@gmail.com>
2025-05-21 21:02:11 +0700

    Added Persisting Data and Improved Teleport

---
+++ SEA Game Project/Assets/Script/GameManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    [Header("Persistent Objects")]
+
+    public GameObject[] persistentObjects;
+
+
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            CleanUpAndDestroy();
+            return;
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            MarkPersistentObjects();
+        }
+    }
+
+    private void MarkPersistentObjects()
+    {
+        foreach(GameObject obj in persistentObjects)
+        {
+            if (obj != null)
+            {
+                DontDestroyOnLoad (obj); 
+            }
+        }
+    }
+
+    private void CleanUpAndDestroy()
+    {
+        foreach (GameObject obj in persistentObjects)
+        {
+            Destroy(obj);
+        }
+
+        Destroy(gameObject);
+    }
+}
--- SEA Game Project/Assets/Script/SceneChange/SceneChanger.cs
+++ SEA Game Project/Assets/Script/SceneChange/SceneChanger.cs
@@ -7,10 +7,13 @@ public class SceneChanger : MonoBehaviour
     public string sceneToLoad;
     public Animator fadeAnim;
     public float fadeTime = .5f;
+    public Vector2 newPlayerPosition;
+    private Transform player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag == "Player")
         {
+            player = collision.transform; 
             fadeAnim.Play("FadeToWhite");
             StartCoroutine(DelayFade());
         }
@@ -18,6 +21,7 @@ public class SceneChanger : MonoBehaviour
     IEnumerator DelayFade()
     {
         yield return new WaitForSeconds(fadeTime);
+        player.position = newPlayerPosition;    
         SceneManager.LoadScene(sceneToLoad);
     }
 }
