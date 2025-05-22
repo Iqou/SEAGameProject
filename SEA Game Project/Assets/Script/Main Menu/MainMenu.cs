3018f0d3c84ade6fc73943da1296c2b869528627
Iqou <inigo.siswanto12@gmail.com>
2025-05-23 00:52:03 +0700

    Play Button

---
+++ SEA Game Project/Assets/Script/Main Menu/MainMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        // You can load by build index or scene name
+        SceneManager.LoadScene("RPG"); // Replace with your actual scene name
+    }
+    public void QuitGame()
+    {
+        // This will quit the application
+        // Note: This won't work in the editor, but will work in a built application
+        // Application.Quit();
+
+        // If you want to stop playing in the editor, uncomment the line below
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #endif
+    }
+}
